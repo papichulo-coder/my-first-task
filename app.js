e44ca2b734664eed3ba872283410e9b8b3fbac92
paulo_codes <iwowopaul66@gmail.com>
2022-10-28 14:58:16 +0100

    my first hng task

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const port = process.env.port || 5000
+
+const data = require('./mydata')
+
+
+
+app.use(cors({
+    origin: '*'
+})); 
+
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+  
+  app.get('/',(req,res,next)=>{
+    res.send(data)
+  })
+
+
+app.listen(port,()=>{
+console.log(`server listening on ${port}`)
+});
---
+++ mydata.js
@@ -0,0 +1,9 @@
+const data=
+    {
+        "slackusername":"paulocodes",
+        "backend":true,
+        "Age":18,
+        "Bio":"just a nigga boy loves writing code,trying to be the best software engineer I can be",
+
+};
+module.exports= {data}
