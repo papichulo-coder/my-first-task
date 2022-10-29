20ace1c1c42e301f19482efbb05eddecf433a536
paulo_codes <iwowopaul66@gmail.com>
2022-10-29 11:52:07 +0100

    Update mydata.js

--- mydata.js
+++ mydata.js
@@ -1,9 +1,1 @@
-const data=
-    {
-        "slackusername":"paulocodes",
-        "backend":true,
-        "Age":18,
-        "Bio":"just a nigga boy loves writing code,trying to be the best software engineer I can be",
 
-};
-module.exports= {data}
